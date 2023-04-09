c0387b231481a2b04218ba4fbae7f13eb6362997
Keks <keks@htmlacademy.ru>
2023-04-09 23:09:16 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/main.js
@@ -0,0 +1,23 @@
+import {renderThumbnails} from './thumbnail.js';
+import {onFormSubmit} from './form.js';
+import {getData} from './api.js';
+import {showAlert, debounce} from './util.js';
+import {closeEditForm} from './form.js';
+import {getFilteredPhoto, init} from './photo-filtering.js';
+import {getUserPhoto} from './user-photo.js';
+
+
+getData()
+  .then((data) => {
+    const debouncedRenderThumbnails = debounce(renderThumbnails);
+    init(data, debouncedRenderThumbnails);
+    renderThumbnails(getFilteredPhoto());
+  }).catch(
+    (err) => {
+      showAlert(err.message);
+    }
+  );
+
+onFormSubmit(closeEditForm);
+
+getUserPhoto();
---
+++ 12/js/thumbnail.js
@@ -0,0 +1,31 @@
+import {getFullSize} from './full-size.js';
+
+const templateThumbnail = document
+  .querySelector('#picture')
+  .content.querySelector('.picture');
+const pictureContainer = document.querySelector('.pictures');
+
+const createThumbnail = (photo) => {
+  const userThumbnail = templateThumbnail.cloneNode(true);
+  userThumbnail.querySelector('.picture__img').src = photo.url;
+  userThumbnail.querySelector('.picture__img').alt = photo.description;
+  userThumbnail.querySelector('.picture__likes').textContent = photo.likes;
+  userThumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
+  userThumbnail.dataset.thumbnailId = photo.id;
+
+  userThumbnail.addEventListener('click', () => getFullSize(photo));
+
+  return userThumbnail;
+};
+
+const renderThumbnails = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictureContainer.querySelectorAll('.picture').forEach((element) => element.remove());
+  pictures.forEach ((picture) => {
+    const userThumbnail = createThumbnail(picture);
+    fragment.append(userThumbnail);
+  });
+  pictureContainer.append(fragment);
+};
+
+export {renderThumbnails};
